dcfe167d96cd8195fd93721e0708e156f87b9e48
PlayerTv9 <tommaso.ibba09@gmail.com>
2023-01-02 22:13:52 +0100

    first commit

--- index.js
+++ index.js
@@ -15,3 +15,20 @@ app.get('/cane',(req,res) => {
 app.listen(5000,() => {
      console.log(`app is listening to port http://localhost:5000`);
 })
+
+app.get('/pitagora',(req, res) =>{
+    res.sendFile(path.join(__dirname, '/static/pages/pitagora.html'));
+})
+
+
+//static files css
+app.get('/icss',(req, res) =>{
+    res.sendFile(path.join(__dirname, '/static/style/index.css'));
+})
+app.get('/navbar',(req, res) =>{
+    res.sendFile(path.join(__dirname, '/static/component/nav.html'));
+})
+
+app.get('/navJS',(req, res) =>{
+    res.sendFile(path.join(__dirname, '/static/script/includeHtml.js'));
+})
