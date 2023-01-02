e137d558c899d4aaf2c7bb04ac7ff96abbe2c5b7
PlayerTv9 <tommaso.ibba09@gmail.com>
2023-01-02 22:29:06 +0100

    fff

--- build/index.js
+++ build/index.js
@@ -9,9 +9,26 @@ app.use(_bodyParser["default"].json());
 app.get('/', function (req, res) {
   res.sendFile(_path["default"].join(__dirname, '/index.html'));
 });
+app.get('/home', function (req, res) {
+  res.sendFile(_path["default"].join(__dirname, '/index.html'));
+});
 app.get('/cane', function (req, res) {
-  res.sendFile(_path["default"].join(__dirname, '/page/cane.html'));
+  res.sendFile(_path["default"].join(__dirname, '/cane.html'));
 });
 app.listen(5000, function () {
   console.log("app is listening to port http://localhost:5000");
+});
+app.get('/pitagora', function (req, res) {
+  res.sendFile(_path["default"].join(__dirname, '/pitagora.html'));
+});
+
+//static files css
+app.get('/icss', function (req, res) {
+  res.sendFile(_path["default"].join(__dirname, '/index.css'));
+});
+app.get('/navbar', function (req, res) {
+  res.sendFile(_path["default"].join(__dirname, '/nav.html'));
+});
+app.get('/navJS', function (req, res) {
+  res.sendFile(_path["default"].join(__dirname, '/includeHtml.js'));
 });
