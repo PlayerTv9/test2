025e433f122831b4f82ddf00bea0069bc0ffaab7
PlayerTv9 <tommaso.ibba09@gmail.com>
2023-01-03 16:41:29 +0100

    kkkkkk

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ app.listen(5000,() => {
 })
 
 app.get('/pitagora',(req, res) =>{
-    res.sendFile(path.join(__dirname, '/files/pages/pitagora.html'));
+    res.sendFile(path.join(__dirname, 'files/pages/pitagora.html'));
 })
 
 
 //static files css
 app.get('/icss',(req, res) =>{
-    res.sendFile(path.join(__dirname, '/files/style/index.css'));
+    res.sendFile(path.join(__dirname, 'files/style/index.css'));
 })
 app.get('/navbar',(req, res) =>{
-    res.sendFile(path.join(__dirname, '/files/component/nav.html'));
+    res.sendFile(path.join(__dirname, 'files/component/nav.html'));
 })
 
 app.get('/navJS',(req, res) =>{
-    res.sendFile(path.join(__dirname, '/files/script/includeHtml.js'));
+    res.sendFile(path.join(__dirname, 'files/script/includeHtml.js'));
 })
